89bd3f99ca422f17c58b47e3537385ae6fa23ca3
Yusuf Syaifudin <yusuf.syaifudin@gmail.com>
2023-04-04 13:31:02 +0700

    add more tracing

--- app/controllers/UserPromotionController.rb
+++ app/controllers/UserPromotionController.rb
@@ -2,66 +2,68 @@ require_dependency "json_web_token"
 
 class UserPromotionController < ApplicationController
 
-    def list_vouchers
-      Rails.logger.debug "request accepted by controller"
+  def list_vouchers
+    Rails.logger.debug "request accepted by controller"
 
-      begin
-          token = ""
-          auth = request.headers['Authorization']
-          if !auth.nil?
-            token = auth.split(' ').last
-          end
-
-          if token.empty?
-            Rails.logger.debug "accepting token from query params"
-
-            token = request.query_parameters['token']
-          else
-            Rails.logger.debug "accepting token from headers"
-          end
-
-          token_extracted = JsonWebToken::decode(token)
-          if token_extracted.nil? || token_extracted.to_a.length() <= 0
-            return respond_to do |format|
-              format.any {render :json => [
-                'message' => 'nil extracted token'
-              ]}
-            end
-          end
-
-
-          jwt_payload = token_extracted[0]
-          if jwt_payload.nil?
-            return respond_to do |format|
-              format.any {render :json => [
-                'message' => 'nil jwt payload'
-              ]}
-            end
-          end
-
-          user_id = jwt_payload['user_id']
-          if user_id.nil? || user_id.empty?
-            return respond_to do |format|
-               format.any {render :json => [
-                 'message' => 'empty user id from jwt payload'
-              ]}
-            end
-          end
-
-          out = Vouchers::UserVoucher::vouchers(user_id)
-          Rails.logger.debug "controller done processing the request, preparing rendering response"
-
-          return respond_to do |format|
-            format.any {render :json => out}
-          end
-      rescue => err
-        return respond_to do |format|
-            format.any  {render :json => [
-                'message': err
-            ]}
+    begin
+      MyAppTracer.in_span("request accepted by controller") do |span|
+        token = ""
+        auth = request.headers['Authorization']
+        if !auth.nil?
+          token = auth.split(' ').last
         end
+      
+        if token.empty?
+          Rails.logger.debug "accepting token from query params"
+      
+          token = request.query_parameters['token']
+        else
+          Rails.logger.debug "accepting token from headers"
+        end
+      
+        token_extracted = JsonWebToken::decode(token)
+        if token_extracted.nil? || token_extracted.to_a.length() <= 0
+          return respond_to do |format|
+            format.any {render :json => [
+              'message' => 'nil extracted token'
+            ]}
+          end
+        end
+      
+      
+        jwt_payload = token_extracted[0]
+        if jwt_payload.nil?
+          return respond_to do |format|
+            format.any {render :json => [
+              'message' => 'nil jwt payload'
+            ]}
+          end
+        end
+      
+        user_id = jwt_payload['user_id']
+        if user_id.nil? || user_id.empty?
+          return respond_to do |format|
+             format.any {render :json => [
+               'message' => 'empty user id from jwt payload'
+            ]}
+          end
+        end
+      
+        out = Vouchers::UserVoucher::vouchers(user_id)
+        Rails.logger.debug "controller done processing the request, preparing rendering response"
+      
+        return respond_to do |format|
+          format.any {render :json => out}
+        end
+      end # end of tracer block
+    rescue => err
+      return respond_to do |format|
+        format.any  {render :json => [
+            'message': err
+        ]}
+      end
 
-      end # end of begin-rescue
-    end
+    end # end of begin-rescue
+  end
 
 end
--- app/services/vouchers/user_voucher.rb
+++ app/services/vouchers/user_voucher.rb
@@ -1,25 +1,30 @@
+require 'opentelemetry/sdk'
+
 module Vouchers
     class UserVoucher
 
        def self.vouchers(user_id)
-           Rails.logger.debug "reaching business logic"
-           Rails.logger.debug "doing query get user by id #{user_id}"
-           user = User::find_by(:id => user_id)
+           MyAppTracer.in_span("list voucher business logic") do |span|
 
-           throw :NotFoundUser if user.nil?
+               Rails.logger.debug "reaching business logic"
+               Rails.logger.debug "doing query get user by id #{user_id}"
+               user = User::find_by(:id => user_id)
 
-           Rails.logger.debug_ctx(
-            "calling PromotionService.list_vouchers from UserVoucher.list_vouchers",
-            nil,
-            {username: user.username},
-           )
+               throw :NotFoundUser if user.nil?
 
-           vouchers = Promotions::PromotionService::list_vouchers()
+               Rails.logger.debug_ctx(
+                "calling PromotionService.list_vouchers from UserVoucher.list_vouchers",
+                nil,
+                {username: user.username},
+               )
 
-           return {
-               'user' => user,
-               'vouchers' => vouchers
-           }
+               vouchers = Promotions::PromotionService::list_vouchers()
+
+               return {
+                   'user' => user,
+                   'vouchers' => vouchers
+               }
+           end
        end
 
     end
